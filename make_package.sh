dfa8df7961d6684b4ca81efa34a6d46436895d3b
Eric Katzfey <eric.katzfey@modalai.com>
2020-12-16 16:33:19 -0800

    Added convenience script to start a UVC camera stream. Fixed version number mismatch.

--- make_package.sh
+++ make_package.sh
@@ -66,6 +66,7 @@ cd build && sudo make DESTDIR=../ipk/data PREFIX=/usr install && cd -
 
 mkdir -p $DATA_DIR/etc/modalai
 cp config/voxl-streamer.conf $DATA_DIR/etc/modalai
+cp script/start-uvc-stream.sh $DATA_DIR/usr/bin
 
 ################################################################################
 # pack the control, data, and final ipk archives
